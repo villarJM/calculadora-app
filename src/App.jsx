7353b2a3274da69914a3ff3b4939f91c40f58350
unknown <l19350325@tuxtepec.tecnm.mx>
2023-03-15 12:18:00 -0600

    Funcionalidad a Botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useState } from 'react';
 import './App.css'
+
+const buttonsClasses = 'btn btn-primary w-75 mt-2';
+
 function App() {
+  const [screen, setScreen] = useState('0');
+  const handleButtonClick = () => {
+    if(screen === '0') {
+      setScreen('9');
+    } else {
+      setScreen(`${screen}9`);
+    }
+  };
 
   return (
     <div className="app">
@@ -12,67 +24,80 @@ function App() {
         <tr>
           <td colSpan={4} style={{
             border: '1px solid black', textAlign: 'end'
-            }}>RESULTADO</td>
+            }}><h2>{screen}</h2></td>
         </tr>
         {/*SECOND ROW*/}
         <tr>
           <td>
             <button 
               type='button'
-              className='btn btn-primary w-75 mt-2'>C</button></td>
+              className={buttonsClasses}>C
+            </button>
+          </td>
           <td>
             <button 
               type='button'
-              className='btn btn-primary w-75 mt-2'>/</button></td>
+              className={buttonsClasses}>/
+            </button>
+          </td>
+          <td>
+            <button 
+              type='button'
+              className={buttonsClasses}>*
+            </button>
+          </td>
           <td><button 
               type='button'
-              className='btn btn-primary w-75 mt-2'>*</button></td>
-          <td><button 
-              type='button'
-              className='btn btn-primary w-75 mt-2'>-</button></td>
+              className={buttonsClasses}>-
+            </button>
+          </td>
         </tr>
         {/*THIRD ROW*/}
         <tr>
+          <td>
+            <button 
+              type='button'
+              className={buttonsClasses}>7
+            </button>
+          </td>
           <td><button 
               type='button'
-              className='btn btn-primary w-75 mt-2'>7</button></td>
+              className={buttonsClasses}>8</button></td>
           <td><button 
               type='button'
-              className='btn btn-primary w-75 mt-2'>8</button></td>
-          <td><button 
-              type='button'
-              className='btn btn-primary w-75 mt-2'>9</button></td>
+              className={buttonsClasses}
+              onClick={handleButtonClick}>9</button></td>
           <td rowSpan={2}><button 
               type='button'
-              className='btn btn-primary w-75 mt-2'
+              className={buttonsClasses}
               style={{height: "85px"}}>+</button></td>
         </tr>
         {/*FOURTH ROW*/}
         <tr>
           <td><button 
               type='button'
-              className='btn btn-primary w-75 mt-2'>4</button></td>
+              className={buttonsClasses}>4</button></td>
           <td><button 
               type='button'
-              className='btn btn-primary w-75 mt-2'>5</button></td>
+              className={buttonsClasses}>5</button></td>
           <td><button 
               type='button'
-              className='btn btn-primary w-75 mt-2'>6</button></td>
+              className={buttonsClasses}>6</button></td>
         </tr>
         {/*FIFTH ROW*/}
         <tr>
           <td><button 
               type='button'
-              className='btn btn-primary w-75 mt-2'>1</button></td>
+              className={buttonsClasses}>1</button></td>
           <td><button 
               type='button'
-              className='btn btn-primary w-75 mt-2'>2</button></td>
+              className={buttonsClasses}>2</button></td>
           <td><button 
               type='button'
-              className='btn btn-primary w-75 mt-2'>3</button></td>
+              className={buttonsClasses}>3</button></td>
           <td rowSpan={2}><button 
               type='button'
-              className='btn btn-primary w-75 mt-2'
+              className={buttonsClasses}
               style={{height: "85px"}}> = </button></td>
         </tr>
         {/*SIXTH ROW*/}
@@ -84,7 +109,7 @@ function App() {
           <td>
             <button 
               type='button'
-              className='btn btn-primary w-75 mt-2'>.</button>
+              className={buttonsClasses}>.</button>
           </td>
         </tr>
       </table>
